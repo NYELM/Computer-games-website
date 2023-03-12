852a8eb0fa6cd71de86ffb2220bff6e18648ec94
Ranyel Queiroz <ranyelqueiroz14@gmail.com>
2023-03-12 15:46:15 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+//efeito do botão voltar ao Topo
+
+function topo (){
+    window.scrollTo(
+        {
+            top:0,
+            left:0,
+            behavior:"smooth"
+        }
+    )
+}
+
+//Validação de Login
+
+function login(){
+    let logado = 0;
+    let usuario = document.getElementById('usuario').value;
+    let senha = document.getElementById('senha').value;
+
+    if(usuario == 'admin' && senha == '123456'){
+        window.location = 'index.html';
+        logado = 1;
+    }
+
+    if(logado == 0){
+        alert('Usuario ou senha incorreto');
+    }
+
+}
+
+//Ativar alert no botão cadastrar
+
+function cadastro(){
+    alert('Cadastrado com sucesso');
+    window.location.href = 'index.html';
+}
